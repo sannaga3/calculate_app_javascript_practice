90a778d2455f34aa74ee1e26d5d65e5ebf9da3a4
sannaga3 <n.tetsuya02@gmail.com>
2021-08-09 13:18:54 +0900

    レスポンス受け取り成功

--- app/controllers/target_scores_controller.rb
+++ app/controllers/target_scores_controller.rb
@@ -1,46 +1,37 @@
 class TargetScoresController < ApplicationController
   before_action :set_target_score, only: %i[ show edit update destroy ]
 
-  # GET /target_scores or /target_scores.json
   def index
     @target_scores = TargetScore.all
   end
 
-  # GET /target_scores/1 or /target_scores/1.json
   def show
   end
 
-  # GET /target_scores/new
   def new
     @students = Student.all
     @target_score = TargetScore.new
   end
 
-  # GET /target_scores/1/edit
   def edit
     @students = Student.all
   end
 
-  # POST /target_scores or /target_scores.json
   def create
-    # binding.irb
-    @target_score = TargetScore.new
+    @target_score = TargetScore.new(target_score_params)
     @students = Student.all
     respond_to do |format|
       if @target_score.save
         format.html { redirect_to @target_score, notice: "Target score was successfully created." }
         format.json { render :show, status: :created, location: @target_score }
-        # format.json { binding.irb }
       else
-        # format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @target_score.errors, status: :unprocessable_entity }
-        # format.json { binding.irb }
-        # format.js { render json: @target_score.errors, status: :unprocessable_entity }
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @target_score.errors, status: 404 }
+        # format.json { render :show, status: :created, location: @target_score }
       end
     end
   end
 
-  # PATCH/PUT /target_scores/1 or /target_scores/1.json
   def update
     respond_to do |format|
       if @target_score.update(target_score_params)
@@ -53,7 +44,6 @@ class TargetScoresController < ApplicationController
     end
   end
 
-  # DELETE /target_scores/1 or /target_scores/1.json
   def destroy
     @target_score.destroy
     respond_to do |format|
@@ -63,12 +53,11 @@ class TargetScoresController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_target_score
       @target_score = TargetScore.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def target_score_params
       params.require(:target_score).permit(:math, :english, :science, :student_id)
     end
--- app/views/target_scores/_target_score.json.jbuilder
+++ app/views/target_scores/_target_score.json.jbuilder
@@ -1,2 +1,19 @@
 json.extract! target_score, :id, :math, :english, :science, :student_id, :created_at, :updated_at
 json.url target_score_url(target_score, format: :json)
+
+# 上と下は記述は違うが同じ表示方法 json.extract!・・・第一引数に指定したインスタンス変数のデータをJSON形式の文字列で返却
+
+# json.id @target_score.id
+# json.math @target_score.math
+# json.english @target_score.english
+# json.science @target_score.science
+# json.created_at @target_score.created_at
+
+
+# json.array! target_scores, :id, :math, :english, :science, :student_id, :created_at
+# json.array!・・・モデルの全てのインスタンスを、配列に格納されたJSON形式の文字列データで返却
+
+
+# json.target_scores do
+#   json.array! @target_scores, :id, :math, :english, :science, :student_id, :created_at
+# end
